cb9c3865b203da4a04f3ff294aec1f29162e3bd0
THEToilet <bp18015@shibaura-it.ac.jp>
2020-03-17 00:26:04 +0900

    ✨ add

--- AtCoder/AtCoderBeginnersSelection/DayDream.cpp
+++ AtCoder/AtCoderBeginnersSelection/DayDream.cpp
@@ -4,14 +4,37 @@ int main()
 {
   string daydream[4] = {"dream", "dreamer", "erase", "eraser"};
   string S;
-  string T = "";
   cin >> S;
   reverse(S.begin(), S.end());
   for (int i = 0; i < 4; i++)
   {
     reverse(daydream[i].begin(), daydream[i].end());
   }
-  if()
-  cout << "YES" << endl;
-  cout << "NO" << endl;
+  bool is = true;
+  for (int i = 0; i < S.size();)
+  {
+  bool isDiv = false;
+    for (int j = 0; j < 4; j++)
+    {
+      string tmp = daydream[j];
+      if (S.substr(i, tmp.size()) == tmp)
+      {
+        isDiv = true;
+        i += tmp.size();
+      }
+    }
+    if (!isDiv)
+    {
+      is = false;
+      break;
+    }
+  }
+  if (is)
+  {
+    cout << "YES" << endl;
+  }
+  else
+  {
+    cout << "NO" << endl;
+  }
 }
