df9e471fe7a5f321eb89786b1fc98a00bd2cf41d
THEToilet <bp18015@shibaura-it.ac.jp>
2020-04-01 20:36:27 +0900

    :sparkles: Add ABC 154

--- AtCoder/ABC/160/C-TravelingSalesmanAroundLake.cpp
+++ AtCoder/ABC/160/C-TravelingSalesmanAroundLake.cpp
@@ -19,21 +19,25 @@ int main()
   for (int i = 0; i < N; i++)
   {
     long int temp = A.at(i);
-    if(i==0){
-      neo.push_back(A.at(i) + K - A.at(N-1));
-      neo.push_back(A.at(i+1)-A.at(i));
+    if (i == 0)
+    {
+      neo.push_back(A.at(i) + K - A.at(N - 1));
+      neo.push_back(A.at(i + 1) - A.at(i));
     }
-    if(i==N-1){
+    if (i == N - 1)
+    {
       break;
     }
-    neo.push_back(A.at(i+1)-A.at(i));
+    neo.push_back(A.at(i + 1) - A.at(i));
   }
   //sort(neo.begin(), neo.end());
 
-  for(int i=0;i<N;i++){
+  for (int i = 0; i < N; i++)
+  {
     cout << neo.at(i) << endl;
   }
-  for(int i=0;i<N-1;i++){
+  for (int i = 0; i < N - 1; i++)
+  {
     sum += neo.at(i);
   }
   cout << sum << endl;
