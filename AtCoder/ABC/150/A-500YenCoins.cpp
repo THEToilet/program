8ab463a8d82953f8e1e3884bb0cd6762fb94fb78
THEToilet <bp18015@shibaura-it.ac.jp>
2020-04-01 21:45:28 +0900

    :sparkles: Add ABC 150

---
+++ AtCoder/ABC/150/A-500YenCoins.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int K, M;
+  cin >> K >> M;
+  if (500 * K >= M)
+  {
+    cout << "Yes" << endl;
+  }
+  else
+  {
+    cout << "No" << endl;
+  }
+}
