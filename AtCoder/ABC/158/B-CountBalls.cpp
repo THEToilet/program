8feaeb0b340572e5e1dfb0947612c7c9efb56dd0
THEToilet <bp18015@shibaura-it.ac.jp>
2020-04-09 19:04:57 +0900

    :sparkles: Add ABC 158 B

--- AtCoder/ABC/158/B-CountBalls.cpp
+++ AtCoder/ABC/158/B-CountBalls.cpp
@@ -1,67 +1,12 @@
 #include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 int main()
 {
-  // Fault
-  long long int N, A, B;
-  int point = 0;
-  string s = "";
+  long long int N;
+  long long int A, B;
   cin >> N >> A >> B;
-  if (A == 0)
-  {
-    cout << 0 << "\n";
-    return 0;
-  }
-  for (int i = 0; i < 100; i++)
-  {
-    for (int j = 0; j < A; j++)
-    {
-      s += "b";
-    }
-    for (int k = 0; k < B; k++)
-    {
-      s += "r";
-    }
-  }
-  if (N < 100)
-  {
-    for (int i = 0; i < N; i++)
-
-    {
-      if (s.at(i) == 'b')
-      {
-        point++;
-      }
-    }
-  }
-  else
-  {
-    for (int i = 0; i < 100; i++)
-    {
-      if (s.at(i) == 'b')
-      {
-        point++;
-      }
-    }
-  }
-  if (N / 100 > 1)
-  {
-    int a = N%100;
-    //cout << a << endl;
-    point = point * (N/100);
-    for (int i = 0; i < a; i++)
-    {
-      if (s.at(i) == 'b')
-      {
-        point++;
-      }
-    }
-    cout << point << "\n";
-
-
-  }
-  else
-  {
-    cout << point << "\n";
-  }
+  long long int a = N / (A + B);
+  long long int b = N % (A + B);
+  cout << a * A + min(A, b) << endl;
 }
