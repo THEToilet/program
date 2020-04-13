dbd25a7a376825eff0405f51461fe4e8f8656e6a
THEToilet <bp18015@shibaura-it.ac.jp>
2020-04-13 17:07:12 +0900

    :sparkles: Add ABC 146 C

--- AtCoder/ABC/146/C-BuyAnInteger.cpp
+++ AtCoder/ABC/146/C-BuyAnInteger.cpp
@@ -5,5 +5,31 @@ int main()
 {
   long int A, B, X;
   cin >> A >> B >> X;
-  
+  long long l = 0, r = 1e9 + 1;
+  // ラムダ関数
+  auto keta = [&](long long c) {
+    int res = 0;
+    while (c)
+    {
+      c /= 10;
+      res++;
+    }
+    return res;
+  };
+  auto f = [&](long long c) {
+    return A * c + B * keta(c);
+  };
+  // 二分探査(差が1になるまで)
+  while (r - l > 1)
+  {
+    long long center = (l + r) / 2;
+    if (f(center) <= X)
+      l = center;
+    else
+    {
+      r = center;
+    }
+  }
+  cout << l << endl;
+  return 0;
 }
