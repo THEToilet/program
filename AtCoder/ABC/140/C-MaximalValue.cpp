bcd12e96e13e9be311eb2b755ffe86e307dd3fc5
THEToilet <bp18015@shibaura-it.ac.jp>
2020-04-16 01:42:22 +0900

    :sparkles: Add ABC 140 C

--- AtCoder/ABC/140/C-MaximalValue.cpp
+++ AtCoder/ABC/140/C-MaximalValue.cpp
@@ -5,9 +5,23 @@ int main()
 {
   int N;
   cin >> N;
-  vector<long int> B(N);
-  rep(i, N)
+  vector<int> B(N - 1);
+  rep(i, N - 1)
   {
     cin >> B[i];
   }
+  vector<int> A(N, 1e12); // infで初期化
+  rep(i, N - 1)
+  {
+    A[i] = min(A[i], B[i]);
+    A[i + 1] = min(A[i + 1], B[i]);
+  }
+
+  int ans = 0;
+  rep(i, N)
+  {
+    ans += A[i];
+  }
+  cout << ans << endl;
+  return 0;
 }
