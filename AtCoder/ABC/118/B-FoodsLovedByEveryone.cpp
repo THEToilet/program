f36e16404fdc6a468d6db9407473978461714547
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-01 18:07:16 +0900

    :sparkles: Add ABC 118 B

---
+++ AtCoder/ABC/118/B-FoodsLovedByEveryone.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+typedef long long ll;
+using namespace std;
+int main()
+{
+  int N, M;
+  cin >> N >> M;
+  vector<int> cnt(M, 0);
+  for (int i = 0; i < N; ++i)
+  {
+    int K;
+    cin >> K;
+    for (int j = 0; j < K; ++j)
+    {
+      int A;
+      cin >> A;
+      --A;
+      ++cnt[A];
+    }
+  }
+  int ans = 0;
+  for (int i = 0; i < M; ++i)
+  {
+    if (cnt[i] == N)
+    {
+      ++ans;
+    }
+  }cd 
+  cout << ans << endl;
+}
