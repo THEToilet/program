2bbf4a750d5b1f9b862264f89d36a0c629bd5aa9
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-03 20:50:44 +0900

    :sparkles: Update 165 D

--- AtCoder/ABC/165/D-FloorFunction.cpp
+++ AtCoder/ABC/165/D-FloorFunction.cpp
@@ -3,24 +3,14 @@
 typedef long long ll;
 using namespace std;
 int main()
-{
+{ // floor(a/b) = (a-a%b)/b
   ll A, B, N;
   cin >> A >> B >> N;
-  //cout << floor(1 / 100) << endl;
-  if (N == 1)
-  {
-    cout << floor((A * 1) / B) << endl;
-    return 0;
+  ll x = N;
+  if(N >= B-1){
+    x = B-1;
   }
-  else if (A <= B)
-  {
-    cout << floor((A * (N - 1)) / B) - A * floor((N - 1) / B) << endl;
-    return 0;
-  }
-  else
-  {
-    cout << floor((A * N) / B) - A * floor(N / B) << endl;
-    return 0;
-  }
+  ll ans = A*(x%B)/B;
+  cout << ans << endl;
   return 0;
 }
