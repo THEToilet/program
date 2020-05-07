087b399d9aed4067083ce4137c9ac07fd3c35606
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-07 22:37:15 +0900

    :sparkles: Update Disco B

--- AtCoder/Disco/B-IronBarCutting.cpp
+++ AtCoder/Disco/B-IronBarCutting.cpp
@@ -2,9 +2,23 @@
 #define rep(i, n) for (int i = 0; i < (n); i++)
 typedef long long ll;
 using namespace std;
+long long N, A[1 << 18]; // 262144
 int main()
 {
-  int N;
   cin >> N;
+  long long S = 0;
+  for (int i = 1; i <= N; i++)
+  {
+    cin >> A[i];
+    S += A[i];
+  }
+
+  long long minx = S, sum = 0;
+  for (int i = 1; i <= N; i++)
+  {
+    sum += A[i];
+    minx = min(minx, abs(sum - (S - sum)));  // 累積和
+  }
+  cout << minx << endl;
   return 0;
 }
