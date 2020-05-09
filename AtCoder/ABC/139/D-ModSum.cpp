e253ede2707b92206c4915dc025d88cabc40566d
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-10 04:24:52 +0900

    :sparkles: Add ABC 139 D

---
+++ AtCoder/ABC/139/D-ModSum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+typedef long long ll;
+using namespace std;
+int main()
+{
+  ll N;
+  cin >> N;
+  ll ans = (N - 1) * N / 2;
+  cout << ans << endl;
+  return 0;
+}
