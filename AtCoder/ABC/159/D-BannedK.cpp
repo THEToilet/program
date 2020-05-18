c4298aa2a47d57c71a726622c80dc6369cba8328
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-18 21:16:27 +0900

    :sparkles: Add ABC 159 D

--- AtCoder/ABC/159/D-BannedK.cpp
+++ AtCoder/ABC/159/D-BannedK.cpp
@@ -1,21 +1,33 @@
 #include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+typedef long long ll;
 using namespace std;
+ll choose2(ll n)
+{
+  return n * (n - 1) / 2;
+}
+
 int main()
 {
-  long int N;
-  vector<long int> A;
-  cin >> N;
-  for (int i = 0; i < N; i++)
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+  rep(i, n) a[i]--;
+  vector<int> cnt(n);
+  rep(i, n) cnt[a[i]]++;
+  ll tot = 0;
+  rep(i, n)
   {
-    long int a;
-    cin >> a;
-    A.push_back(a);
+    tot += choose2(cnt[i]);
   }
-  for (int i = 0; i < N; i++)
+  rep(i, n)
   {
-    for (int k = 0; k < N; k++)
-    {
-      A.at(i);
-    }
+    ll ans = tot;
+    ans -= choose2(cnt[a[i]]);
+    ans += choose2(cnt[a[i]] - 1);
+    // - 3C2 + 2C2
+    cout << ans << endl;
   }
+  return 0;
 }
