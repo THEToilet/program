e6fd7e9650eaea063d7954200330cf22602e22f3
THEToilet <bp18015@shibaura-it.ac.jp>
2020-05-30 23:46:22 +0900

    :sparkles: NOMURA

---
+++ AtCoder/NOMURA/A-StudyScheduling.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+typedef long long ll;
+using namespace std;
+int main()
+{
+  int H1, M1, H2, M2, K;
+  cin >> H1 >> M1 >> H2 >> M2 >> K;
+  int h, m;
+  h = (H2 - H1) * 60;
+  m = (M2 - M1) - 1;
+  if (H2 < H1)
+  {
+    h = (24 + H2 - H1) * 60;
+    cout << ((h + m) / K) * K << endl;
+  }
+  else
+  {
+    cout << ((h + m) / K) * K << endl;
+  }
+  return 0;
+}
